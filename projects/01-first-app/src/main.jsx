d59457da71b0e7cb986d46db72ca60fe8b5a7551
cfgonzalez <cfgonzalez@gmail.com>
2024-11-30 18:17:06 -0300

    01-first-app:  Uso de custom font

--- projects/01-first-app/src/main.jsx
+++ projects/01-first-app/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import '@src/styles/global.scss';
+
 
 const root = createRoot(document.getElementById('app'));
 root.render(<App />);
