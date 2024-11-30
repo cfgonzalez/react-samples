10ef48592f36cf26668926803cb3c0d032d1ac7f
cfgonzalez <cfgonzalez@gmail.com>
2024-11-30 18:08:31 -0300

    01-first-app: Children al 50% customizados desde el container

--- projects/01-first-app/src/components/principal-container/PrincipalContainer.jsx
+++ projects/01-first-app/src/components/principal-container/PrincipalContainer.jsx
@@ -2,13 +2,15 @@ import styles from './PrincipalContainer.module.scss';
 
 import React from "react";
 import { LeftContainer } from "../left-container/LeftContainer";
+import { RightContainer } from '../right-container/RightContainer';
 
 export const PrincipalContainer = () => {
   return (
     <div className={styles.container}>
       <div>Principal Container</div>
-      <div>
+      <div className={styles.content}>
         <LeftContainer />
+        <RightContainer />
       </div>
     </div>
   );
---
+++ projects/01-first-app/src/components/right-container/RightContainer.jsx
@@ -0,0 +1,9 @@
+import styles from './RightContainer.module.scss';
+
+import React from "react";
+
+export const RightContainer = () => {
+  return (
+    <div className={styles.container}>Right Container</div>
+  );
+}
