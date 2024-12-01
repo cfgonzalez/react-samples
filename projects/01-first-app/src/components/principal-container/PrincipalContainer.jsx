f887f8bde4403b11134481c5afd533124db51cc2
cfgonzalez <cfgonzalez@gmail.com>
2024-11-30 21:01:50 -0300

    01-first-app: Component Form y estilos grises

--- projects/01-first-app/src/components/principal-container/PrincipalContainer.jsx
+++ projects/01-first-app/src/components/principal-container/PrincipalContainer.jsx
@@ -3,11 +3,13 @@ import styles from './PrincipalContainer.module.scss';
 import React from "react";
 import { LeftContainer } from "../left-container/LeftContainer";
 import { RightContainer } from '../right-container/RightContainer';
+import { PersonForm } from '../person-form/PersonForm';
 
 export const PrincipalContainer = () => {
   return (
     <div className={styles.container}>
       <div>Principal Container</div>
+      <PersonForm width="300px" />
       <div className={styles.content}>
         <LeftContainer />
         <RightContainer />
