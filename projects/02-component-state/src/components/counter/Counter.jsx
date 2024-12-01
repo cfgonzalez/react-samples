630112908b0d1dd729edc875a687378cb58ed7b8
cfgonzalez <cfgonzalez@gmail.com>
2024-12-01 09:20:25 -0300

    02-component-state: Ejemplos de actualizacion de estado

--- projects/02-component-state/src/components/counter/Counter.jsx
+++ projects/02-component-state/src/components/counter/Counter.jsx
@@ -17,11 +17,31 @@ export const Counter = () => {
     setCount(count + 1);
   }
 
+  function handleAddPlus3Click() {
+    // Solo se actualiza una vez ya que react "encola" los set y solo cambia en el proximo "rerender"
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+  }
+
+  function handleAddAndAlert() {
+    // Esto no funcionaria ya que react siempre trabaja con un snapshot del state durante el render
+    // setCount(count + 5);
+    // alert(count);
+
+    // En su lugar hay que hacer esto
+    let newCount = count + 5;
+    setCount(newCount);
+    alert(newCount);
+  }
+
   return (
     <div>
       <div>Last render: {formattedTime}</div>
       <div>Current Count: {count}</div>
       <button onClick={handleAddClick}>Add</button>
+      <button onClick={handleAddPlus3Click}>Add + 3</button>
+      <button onClick={handleAddAndAlert}>Add +5 and Alert</button>
     </div>
   );
 }
